24c5db45038235dcfd332d2fd72687b3473db616
rashmi <rashmi@Rashmis-MacBook-Pro.local>
2014-12-19 00:29:29 -0800

    exporting scoop path in shell script

--- dezyre-players.sh
+++ dezyre-players.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-export JAVA_HOME="$(/usr/libexec/java_home)" && pig /Users/rashmi/dezyre-players/pig/script/players.pig && /Users/rashmi/sqoop-1.4.5.bin__hadoop-2.0.4-alpha/bin/sqoop export --connect jdbc:mysql://localhost/players --table players_data --export-dir /user/rashmi/players/output --username root --input-fields-terminated-by '\t' --input-lines-terminated-by '\n'
+export PATH=$PATH:/Users/rashmi/sqoop-1.4.5.bin__hadoop-2.0.4-alpha/bin/
+export JAVA_HOME="$(/usr/libexec/java_home)"
+ 
+pig /Users/rashmi/dezyre-players/pig/script/players.pig && \
+sqoop export --connect jdbc:mysql://localhost/players --table players_data --export-dir /user/rashmi/players/output --username root --input-fields-terminated-by '\t' --input-lines-terminated-by '\n'
