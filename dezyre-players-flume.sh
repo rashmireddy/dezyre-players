ee840260bcc9c92e28b7a70ea6e1c9a8ebb5b5dc
rashmi <rashmi@Rashmis-MacBook-Pro.local>
2014-12-19 12:49:42 -0800

    Adding quick demo youtube video

--- dezyre-players-flume.sh
+++ dezyre-players-flume.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=$PATH:/Users/rashmi/apache-flume-1.5.2-bin/bin
 
-/Users/rashmi/apache-flume-1.5.2-bin/bin/flume-ng agent -n agent -c conf -f /Users/rashmi/apache-flume-1.5.2-bin/conf/flume-conf.properties
+flume-ng agent -n agent -c conf -f /Users/rashmi/apache-flume-1.5.2-bin/conf/flume-conf.properties
 
